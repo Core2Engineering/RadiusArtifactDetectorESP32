7a790a7c230e4f16bbba6dcc8f0b7e0401174087
VintagePentiumGaming <121701224+VintagePentiumGaming@users.noreply.github.com>
2024-09-05 12:25:42 -0400

    Updated code to dual core, giving led's an opportunity to shine

--- WiFiClientConnect/WiFiClientConnect.ino
+++ WiFiClientConnect/WiFiClientConnect.ino
@@ -14,11 +14,60 @@
 const char *ssid = "ArtifactDetector";
 const char *password = "bruhdontconnecttothis556";
 
-int btnGPIO = 0;
-int btnState = false;
 int off = 20;
 int on = 150;
-
+int btn = 33;
+int flash = 15;
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+void Task1code( void *pvParameters ) {
+  for(;;){
+  int avg = 0;
+  int reading = 0;
+  for (int i = 0; i <= 20; i++) {
+    avg += WiFi.RSSI();
+  }
+  reading = avg / 20;
+  //reading = map(reading, -30, -100, 0, 100);
+  if (reading < -60){
+  analogWrite(A5,on);
+  delay(abs(map(reading,-61,-90,50,100)));
+  analogWrite(A5,off);
+  delay(abs(map(reading,-61,-90,50,100)));
+}else if (reading < -30) {
+  analogWrite(A5,on);
+  delay(abs(map(reading,-30,-60,10,50)));
+  analogWrite(A5,off);
+  delay(abs(map(reading,-30,-60,10,50)));
+}
+else{
+  analogWrite(A5,on);
+  delay(5);
+  analogWrite(A5,off);
+  delay(5);
+}
+  tone(A11, 4000);//following is code for the click
+  delay(2);
+  noTone(A11);
+  //Serial.println(reading);
+  //analogWrite(A5,map(reading,-30,-90,255,0));  //testing, use for led brightness in place of blinking
+  //delay(100); 
+  
+}
+}
+void Task2code( void *pvParameters ){
+  for(;;){
+  if (digitalRead(btn) == false){
+      digitalWrite(flash, HIGH);
+      delay(100);
+    for(int i = 255; i>= 0; i--){
+      analogWrite(A8, i);
+      Serial.println(i);
+      delay(3);
+    }
+  }
+}
+}
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -26,6 +75,10 @@ void setup() {
   // Set GPIO0 Boot button as input
   pinMode(A5, OUTPUT);
   pinMode(A11, OUTPUT);
+  pinMode(btn, INPUT_PULLUP);
+  pinMode(flash, OUTPUT);
+  pinMode(A8,OUTPUT);
+  digitalWrite(flash, LOW);
   tone(A11, 4000);//following is code for the click
   delay(2);
   noTone(A11);
@@ -44,7 +97,20 @@ void setup() {
   // Will try for about 10 seconds (20x 500ms)
   int tryDelay = 500;
   int numberOfTries = 20;
+  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(Task1code,"Task1",10000,NULL,1,&Task1,0);           
+  delay(500); 
 
+  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+                    Task2code,   /* Task function. */
+                    "Task2",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task2,      /* Task handle to keep track of created task */
+                    1);          /* pin task to core 1 */
+    delay(500); 
   // Wait for the WiFi event
   while (true) {
 
@@ -81,36 +147,4 @@ void setup() {
   }
 }
 
-void loop() {
-  int avg = 0;
-  int reading = 0;
-  for (int i = 0; i <= 20; i++) {
-    avg += WiFi.RSSI();
-  }
-  reading = avg / 20;
-  //reading = map(reading, -30, -100, 0, 100);
-  if (reading < -60){
-  analogWrite(A5,on);
-  delay(abs(map(reading,-61,-90,50,100)));
-  analogWrite(A5,off);
-  delay(abs(map(reading,-61,-90,50,100)));
-}else if (reading < -30) {
-  analogWrite(A5,on);
-  delay(abs(map(reading,-30,-60,10,50)));
-  analogWrite(A5,off);
-  delay(abs(map(reading,-30,-60,10,50)));
-}
-else{
-  analogWrite(A5,on);
-  delay(5);
-  analogWrite(A5,off);
-  delay(5);
-}
-  tone(A11, 4000);//following is code for the click
-  delay(2);
-  noTone(A11);
-  Serial.println(reading);
-  //analogWrite(A5,map(reading,-30,-90,255,0));  //testing, use for led brightness in place of blinking
-  //delay(100); 
-  
-}
+void loop() { }
